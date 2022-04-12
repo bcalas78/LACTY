59905508a01570e9eba73d62229e14bcfe6f100d
Barbara Calas <barbara.calas@orange.fr>
2022-04-12 12:51:48 +0200

    Reviews & pages infos

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: 'pages#home'
   get "search", to: "products#search"
+  get "information", to: "pages#information"
+  get "board", to: "pages#board"
   resources :products, except: :destroy do
     resources :reviews, only: :create
   end
-  # resources :reviews, except: [:show, :create]
+  resources :reviews, except: :create
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
