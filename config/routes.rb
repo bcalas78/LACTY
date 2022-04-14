fc27d66ba131657af9b83e0fd831248321a4c8a7
Madou Tounkara <tmadou69@icloud.com>
2022-04-14 16:15:58 +0200

    fin de page presentation

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'search', to: "products#search"
   resources :products, only: [:new, :show, :create, :index]
+  get 'presentation', to: "pages#presentation_equipe"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
